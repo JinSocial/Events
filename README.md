f935ecbfcc9ad6489fe606b269b86259ed24ea96
GlittersIsGold <cyberk1ra@ya.ru>
2023-03-17 11:50:04 +0300

    sql update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # JinEvents
-Web app to create and participate events or community, solve problems and discovering something new 
+
+Web app to create and participate events or community, solve problems and discovering something new
+
+# Technology stack
+
+ <img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" title="CSharp" alt="CSharp" width="40" height="40"/>&nbsp;
+ <img src="https://github.com/devicons/devicon/blob/master/icons/csharp/csharp-original.svg" title="CSharp" alt="CSharp" width="40" height="40"/>&nbsp;
+ <img src="https://github.com/devicons/devicon/blob/master/icons/postgresql/postgresql-original.svg" title="PostgreSQL" alt="PostgreSQL" width="40" height="40"/>&nbsp;
--- server/DbScript.sql
+++ server/DbScript.sql
@@ -1,39 +1,40 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  email VARCHAR(256) NOT NULL,
+  email VARCHAR(64) NOT NULL,
   password VARCHAR(64) NOT NULL,
-  name VARCHAR(64),
-  avatar VARCHAR(256),
+  login VARCHAR(64),
+  img_path VARCHAR(64),
   about TEXT
 );
 
 CREATE TABLE projects (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(256),
+  title VARCHAR(64) NOT NULL,
   description TEXT,
   point POINT NOT NULL,
-  img VARCHAR(256),
-  creation_date TIMESTAMP
+  img_path VARCHAR(64),
+  creation_date TIMESTAMP NOT NULL
 );
 
 CREATE TABLE project_members (
   project_id INT REFERENCES projects(id) NOT NULL,
   user_id INT REFERENCES users(id) NOT NULL,
-  role VARCHAR(64),
+  role VARCHAR(64) NOT NULL,
   PRIMARY KEY (project_id, user_id)
 );
 
 CREATE TABLE events (
   id SERIAL PRIMARY KEY,
   project_id INT REFERENCES projects(id) NOT NULL,
-  title VARCHAR(256),
-  date TIMESTAMP
+  title VARCHAR(64) NOT NULL,
+  date TIMESTAMP NOT NULL,
+  state BOOLEAN NOT NULL
 );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   user_id INT REFERENCES users(id) NOT NULL,
   project_id INT REFERENCES projects(id) NOT NULL,
-  message TEXT,
-  date TIMESTAMP
+  message TEXT NOT NULL,
+  date TIMESTAMP NOT NULL
 );
